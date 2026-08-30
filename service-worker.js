3286497eaa548a34260d06fbe04a1f6b4db05d40
jasonlombard26 <jason_lombard@outlook.com>
2026-08-30 16:22:17 +1000

    Force installed mobile app cache reset

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,11 @@
+const PPM_CACHE_RESET_VERSION='36';
 self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.registration.unregister();await self.clients.claim();})()));
+self.addEventListener('activate',event=>event.waitUntil((async()=>{
+  for(const key of await caches.keys())await caches.delete(key);
+  const clients=await self.clients.matchAll({type:'window',includeUncontrolled:true});
+  await self.registration.unregister();
+  await self.clients.claim();
+  for(const client of clients){
+    try{await client.navigate(new URL('./?v='+PPM_CACHE_RESET_VERSION,self.registration.scope).href);}catch(_){}
+  }
+})()));
